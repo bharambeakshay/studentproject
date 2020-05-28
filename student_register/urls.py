6621422545ec645acb0ab28f3a7413b8b9b222c2
bharambeakshay <55834039+bharambeakshay@users.noreply.github.com>
2020-05-29 00:48:10 +0530

    some issues regarding bootstrap

--- student_register/urls.py
+++ student_register/urls.py
@@ -2,6 +2,8 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.student_form),#localhost
-    path('list/',views.student_list)
+    path('',views.student_form, name = "student_insert"),#localhost , get and podt for insert operation
+    path('<int:id>/',views.student_form, name = "student_update"), # get and podt for update operation
+    path('<int:id>/',views.student_delete, name = "student_delete"), #for deleting the employee
+    path('list/',views.student_list) #To retrieve and display all records 
 ]
--- studentproject/settings.py
+++ studentproject/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'student_register',
     #it controls forms using bootstrap etc.
-    'crispy_forms',
+    'crispy_forms', 
+    #mobile
+    'phone_field' #you can use this
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap'
 
