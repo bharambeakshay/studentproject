ee48a414c2db47239bf029354393c11da75abf86
bharambeakshay <55834039+bharambeakshay@users.noreply.github.com>
2020-05-29 12:40:09 +0530

    deletion operation is not working

--- student_register/urls.py
+++ student_register/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.student_form, name = "student_insert"),#localhost , get and podt for insert operation
     path('<int:id>/',views.student_form, name = "student_update"), # get and podt for update operation
-    path('<int:id>/',views.student_delete, name = "student_delete"), #for deleting the employee
-    path('list/',views.student_list) #To retrieve and display all records 
+    path('delete/<int:id>/',views.student_delete, name = "student_delete"), #for deleting the student
+    path('list/',views.student_list,name = "student_list") #To retrieve and display all records 
 ]
